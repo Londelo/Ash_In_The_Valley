88430a883c60d69f539be825002511636691a678
TM_Brodie <brodie.balser@ticketmaster.com>
2025-06-29 01:37:33 -0400

    ASH-8: fine tuned attack configs

--- src/game/actors/Player/attackConfigs.ts
+++ src/game/actors/Player/attackConfigs.ts
@@ -51,7 +51,7 @@ const swordsMasterAttackConfigs: PlayerAttackConfigFn =
 const bloodSwordsManAttackConfigs: PlayerAttackConfigFn =
   (_this: Player) => ({
     [`${_this.playerSkin}_player_attack_1`]: {
-      width: 250,
+      width: 150,
       height: 50,
       offsetX_right: 75,
       offsetX_left: -75,
@@ -61,7 +61,7 @@ const bloodSwordsManAttackConfigs: PlayerAttackConfigFn =
       attackerId: 'player'
     },
     [`${_this.playerSkin}_player_attack_2`]: {
-      width: 280,
+      width: 150,
       height: 60,
       offsetX_right: 85,
       offsetX_left: -85,
@@ -81,13 +81,13 @@ const bloodSwordsManAttackConfigs: PlayerAttackConfigFn =
       attackerId: 'player'
     },
     [`${_this.playerSkin}_player_slam_attack`]: {
-      width: 300,
-      height: 120,
-      offsetX_right: 100,
-      offsetX_left: -100,
-      offsetY: -60,
-      duration: 350,
-      damage: _this.attackPower * 1.5,
+      width: 220,
+      height: 80,
+      offsetX_right: 0,
+      offsetX_left: -0,
+      offsetY: -40,
+      duration: 450,
+      damage: _this.attackPower * 1.8,
       attackerId: 'player'
     }
   });
@@ -96,11 +96,11 @@ const lordOfFlamesAttackConfigs: PlayerAttackConfigFn =
   (_this: Player) => ({
     [`${_this.playerSkin}_player_attack_1`]: {
       width: 180,
-      height: 35,
+      height: 60,
       offsetX_right: 55,
       offsetX_left: -55,
-      offsetY: -15,
-      duration: 220,
+      offsetY: -30,
+      duration: 400,
       damage: _this.attackPower * 0.9,
       attackerId: 'player'
     },
@@ -110,17 +110,17 @@ const lordOfFlamesAttackConfigs: PlayerAttackConfigFn =
       offsetX_right: 65,
       offsetX_left: -65,
       offsetY: -20,
-      duration: 250,
+      duration: 400,
       damage: _this.attackPower * 1.1,
       attackerId: 'player'
     },
     [`${_this.playerSkin}_player_attack_3`]: {
-      width: 300,
+      width: 400,
       height: 60,
-      offsetX_right: 80,
-      offsetX_left: -80,
-      offsetY: -30,
-      duration: 400,
+      offsetX_right: 150,
+      offsetX_left: -150,
+      offsetY: -40,
+      duration: 800,
       damage: _this.attackPower * 1.6,
       attackerId: 'player'
     },
@@ -144,7 +144,7 @@ const holySamuraiAttackConfigs: PlayerAttackConfigFn =
       offsetX_right: 70,
       offsetX_left: -70,
       offsetY: -25,
-      duration: 200,
+      duration: 400,
       damage: _this.attackPower * 1.0,
       attackerId: 'player'
     },
@@ -154,15 +154,15 @@ const holySamuraiAttackConfigs: PlayerAttackConfigFn =
       offsetX_right: 75,
       offsetX_left: -75,
       offsetY: -28,
-      duration: 220,
+      duration: 400,
       damage: _this.attackPower * 1.2,
       attackerId: 'player'
     },
     [`${_this.playerSkin}_player_attack_3`]: {
-      width: 350,
+      width: 150,
       height: 80,
-      offsetX_right: 90,
-      offsetX_left: -90,
+      offsetX_right: 60,
+      offsetX_left: -60,
       offsetY: -40,
       duration: 500,
       damage: _this.attackPower * 2.0,
