1f4e286eb30ca21fde18a246733cc3771b4032ef
Levi Shafter <55813522+Levitiku5@users.noreply.github.com>
2025-06-29 20:50:05 -0600

    AvenWood Prophet Boss Fight Trigger

--- src/game/scenes/gehennaDeep/index.ts
+++ src/game/scenes/gehennaDeep/index.ts
@@ -2,7 +2,6 @@ import config from './config';
 import { EventBus } from '../../EventBus';
 import { Scene } from 'phaser';
 import { Player } from '../../actors/Player';
-import { Prophet } from '../../actors/Prophet';
 import { TileMapComponent } from '../../components/TileMap';
 
 export default class GehennaDeep extends Scene {
@@ -11,7 +10,6 @@ export default class GehennaDeep extends Scene {
   world: Phaser.Physics.Arcade.StaticGroup;
 
   player: Player;
-  prophet: Prophet;
   tileMapComponent: TileMapComponent;
   uiText: Phaser.GameObjects.Text;
 
@@ -32,15 +30,10 @@ export default class GehennaDeep extends Scene {
     const playerSpawn: any = this.tileMapComponent.getObjectLayer('spawn')?.objects[0];
 
     this.player = new Player(this, playerSpawn.x * tileMapConfig.scale, playerSpawn.y * tileMapConfig.scale);
-    
-    // Create the Prophet NPC in the center of the cave
-    this.prophet = new Prophet(this, mapWidth / 2, playerSpawn.y * tileMapConfig.scale, this.player);
 
     this.player.create();
-    this.prophet.create();
 
     this.createUI();
-    this.setupCombat();
 
     this.camera.startFollow(this.player.sprite);
     this.camera.setFollowOffset(0, 200);
@@ -48,7 +41,6 @@ export default class GehennaDeep extends Scene {
     this.camera.setBounds(0, 0, mapWidth, mapHeight);
 
     this.physics.add.collider(this.player.sprite, this.world);
-    this.physics.add.collider(this.prophet.sprite, this.world);
 
     EventBus.emit('current-scene-ready', this);
   }
@@ -70,7 +62,7 @@ export default class GehennaDeep extends Scene {
     const playerHealthPercent = Math.max(0, (this.player.health / this.player.maxHealth) * 100);
     
     this.uiText.setText([
-      `Gehenna Deep - The Prophet's Domain`,
+      `Gehenna Deep - The Ancient Caverns`,
       `Player Health: ${playerHealthPercent.toFixed(0)}%`,
       '',
       'Controls:',
@@ -82,73 +74,13 @@ export default class GehennaDeep extends Scene {
       'Q - Dash',
       'W - Block',
       '',
-      'Find the corrupted Prophet...',
-      'Attack him to begin the final confrontation!'
+      'Explore the mysterious depths...',
+      'Return to Aven Wood to face the Prophet!'
     ]);
-  }
-
-  private setupCombat() {
-    // Player attacks Prophet - this triggers the boss fight
-    this.physics.add.overlap(
-      this.player.sprite,
-      this.prophet.sprite,
-      () => {
-        // Check for active player hitboxes
-        const activeHitboxes = this.player.attackHitboxManager.getActiveHitboxes();
-        activeHitboxes.forEach(hitbox => {
-          if (hitbox.isActive && this.physics.overlap(hitbox.sprite, this.prophet.sprite)) {
-            // Prophet has been attacked - trigger boss fight transformation
-            this.triggerBossFight();
-            hitbox.destroy();
-          }
-        });
-      }
-    );
-  }
-
-  private triggerBossFight() {
-    // Show transformation message
-    const transformText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY - 100, 
-      'The Prophet transforms into something terrible...', {
-      fontSize: '24px',
-      color: '#ff0000',
-      fontStyle: 'bold',
-      backgroundColor: '#000000',
-      padding: { x: 20, y: 10 }
-    });
-    transformText.setOrigin(0.5);
-    transformText.setScrollFactor(0);
-    transformText.setDepth(2000);
-
-    // Add dramatic effect
-    this.cameras.main.shake(1000, 0.02);
-    
-    // Flash effect
-    this.cameras.main.flash(500, 255, 0, 0);
-
-    // Transition to boss fight after dramatic pause
-    this.time.delayedCall(2000, () => {
-      this.scene.start('BossFight');
-    });
   }
 
   update(time: number, delta: number) {
     this.player.update(time, delta);
-    this.prophet.update(time, delta);
     this.updateUI();
-    this.updateCombatOverlaps();
-  }
-
-  private updateCombatOverlaps() {
-    // Continuously check for combat overlaps
-    const playerHitboxes = this.player.attackHitboxManager.getActiveHitboxes();
-
-    // Player hitting Prophet
-    playerHitboxes.forEach(hitbox => {
-      if (hitbox.isActive && this.physics.overlap(hitbox.sprite, this.prophet.sprite)) {
-        this.triggerBossFight();
-        hitbox.destroy();
-      }
-    });
   }
 }
